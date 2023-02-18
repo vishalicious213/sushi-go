0057aaf3228a4ad1628f93a68a520a9754c7cf00
vishalicious213 <vishalicious213@gmail.com>
2023-02-18 03:41:22 -0800

    js: grabbed #start-game, #add-player-btn, #start-game-btn and #players-setup-modal

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-console.log("Sushi Go Scoreboard")
+const startGameSection = document.getElementById("start-game")
+const addPlayerBtn = document.getElementById("add-player-btn")
+const startGameBtn = document.getElementById("start-game-btn")
+const playersSetupModal = document.getElementById("players-setup-modal")
+

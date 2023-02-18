5d3338856bf3c3173f9b61fa344b30aff28a0d29
vishalicious213 <vishalicious213@gmail.com>
2023-02-18 04:34:51 -0800

    submitPlayerBtn gets value from addPlayerNameInput

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
+// START-GAME SECTION
 const startGameSection = document.getElementById("start-game")
 const addPlayerBtn = document.getElementById("add-player-btn")
 const startGameBtn = document.getElementById("start-game-btn")
+// PLAYERS-SETUP-MODAL
 const playersSetupModal = document.getElementById("players-setup-modal")
 const addPlayerDoneBtn = document.getElementById("add-player-done-btn")
+// ADD-PLAYER-FORM
+const addPlayerNameInput = document.getElementById("add-player-name")
+const submitPlayerBtn = document.getElementById("submit-player-btn")
 
 // ⬇️ USER INTERFACE ⬇️
 
@@ -19,4 +24,9 @@ addPlayerDoneBtn.addEventListener("click", function() {
     playersSetupModal.classList.remove("flex")
     startGameSection.classList.remove("none")
     startGameSection.classList.add("flex")
+})
+
+submitPlayerBtn.addEventListener("click", function(e) {
+    e.preventDefault()
+    console.log(addPlayerNameInput.value)
 })

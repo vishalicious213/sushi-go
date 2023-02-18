2926f76a8a133aa4e4f229f920751035464d81c9
vishalicious213 <vishalicious213@gmail.com>
2023-02-18 03:56:30 -0800

    clicking #add-player-done-btn hides #players-setup-modal and shows #start-game

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const startGameSection = document.getElementById("start-game")
 const addPlayerBtn = document.getElementById("add-player-btn")
 const startGameBtn = document.getElementById("start-game-btn")
 const playersSetupModal = document.getElementById("players-setup-modal")
+const addPlayerDoneBtn = document.getElementById("add-player-done-btn")
 
 // ⬇️ USER INTERFACE ⬇️
 
@@ -10,4 +11,12 @@ addPlayerBtn.addEventListener("click", function() {
     startGameSection.classList.add("none")
     playersSetupModal.classList.remove("none")
     playersSetupModal.classList.add("flex")
+})
+
+addPlayerDoneBtn.addEventListener("click", function() {
+    console.log("Done Adding Players")
+    playersSetupModal.classList.add("none")
+    playersSetupModal.classList.remove("flex")
+    startGameSection.classList.remove("none")
+    startGameSection.classList.add("flex")
 })
